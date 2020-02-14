0a11f4ec536fdeddb704617cee2acd5de584bf24
Nicolas Pennec <nicolas.pennec@scaledynamics.io>
2020-02-14 15:31:40 +0000

    Add "HTTP Proxy" sample

---
+++ http-proxy/README.md
@@ -0,0 +1,5 @@
+# Sample - HTTP Proxy
+
+A simple HTTP proxy with [WarpJS](https://warpsjs.com).
+
+See documentation on [warpjs.dev](https://warpsjs.dev/docs/getting-started)
---
+++ http-proxy/src/index.js
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2020 ScaleDynamics SAS. All rights reserved.
+ * Licensed under the MIT license.
+ */
+
+'use strict'
+
+// import WarpJS
+import { defaultWarper as warper } from '@warpjs/warp'
+import engine from '@warpjs/engine'
+
+// init WarpJS
+engine.init()
+
+// warp function
+const fetchUsers = async () => {
+  'warp +server -client'
+
+  const axios = require('axios')
+
+  // fetch users from API
+  const { data } = await axios.get('https://jsonplaceholder.typicode.com/users')
+
+  return data
+}
+
+// on load
+warper.call(fetchUsers).then(response => {
+  result.innerHTML += `
+    <h2>User List</h2>
+    <div>${response.map(user => `${user.id} - ${user.name}`).join('<br>')}</div>
+  `
+})
