0fda3ba0b2f37005a013d6feb8e7d68011e307cd
Erwan Morvillez <erwan.morvillez@scaledynamics.io>
2020-03-13 16:40:40 +0000

    Add Firebase Authentication

--- README.md
+++ README.md
@@ -14,6 +14,7 @@ To learn how to get started with WarpJS, follow our [Getting Started](https://wa
 
 - [Contact-us form with Vue.js](./contact-us) - Create a simple contact-us form in Vue.js and send e-mail with Node.js thanks to WarpJS.
 - [Dark Sky API](./darksky) - Get the weather from the Dark Sky REST API
+- [Firebase Authentication](./firebase-auth) - Secure server function with Firebase and WarpJS
 - [Hello World](./hello-world) - A quickstart sample from the [Getting Started](https://warpjs.dev/docs/getting-started)
 - [HTTP Proxy](./http-proxy) - A simple HTTP Proxy with Axios. Project from the [Getting Started (step 2)](https://warpjs.dev/docs/nodejs-modules)
 - [MongoDB Movies](./mongodb-movies) - Fetch a movie database with WarpJS and MongoDB driver for Node.js
---
+++ firebase-auth/server/index.js
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2020 ScaleDynamics SAS. All rights reserved.
+ * Licensed under the MIT license.
+ */
+const MongoClient = require('mongodb').MongoClient
+const firebaseAdmin = require('firebase-admin')
+
+const MONGODB_URI = 'mongodb+srv://test:test@movies-scqxj.gcp.mongodb.net/'
+
+// initialize firebase admin
+const serviceAccount = require('./firebase-admin-config.json')
+
+firebaseAdmin.initializeApp({
+  credential: firebaseAdmin.credential.cert(serviceAccount)
+})
+
+/**
+ * return list of movies from Mongo Database with search criterion
+ * @param {string} search search criterion
+ */
+const fetchMovies = async (search) => {
+  let mongoClient
+  try {
+    // open a database connection
+    mongoClient = await MongoClient.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+  } catch (error) {
+    throw new Error(`Database connection failed (${error.message})`)
+  }
+
+  // query the database
+  return mongoClient
+    .db('sample_mflix')
+    .collection('movies')
+    .find({ poster: { $exists: true }, title: { $regex: search, $options: 'i' } })
+    .project({ _id: 0, title: 1, year: 1, plot: 1, poster: 1 })
+    .sort({ year: -1 })
+    .limit(50)
+    .toArray()
+}
+
+/**
+ * Check that user is logged. Client sent the firebase user token
+ * and server verify that token is valid
+ * @param {string} idToken Firebase user token
+ */
+const checkUser = async (idToken) => {
+  const userForbiddenError = new Error('User not found!')
+  if (!idToken) {
+    throw userForbiddenError
+  }
+  try {
+    await firebaseAdmin.auth().verifyIdToken(idToken)
+    return true
+  } catch (error) {
+    console.log('error on server', error)
+    throw userForbiddenError
+  }
+}
+
+module.exports = {
+  checkUser,
+  fetchMovies
+}
